e3826fa6107a639904763f5fe0e1219ebc84660c
Pierre-Yves Rollo <dev@pyrollo.com>
2018-12-09 17:17:14 +0100

    Added some Scifi monitors

--- functions.lua
+++ functions.lua
@@ -18,8 +18,6 @@
     along with signs.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-digiterms.columns = 30
-
 local function get_lines(pos)
   local lines = {}
   local meta = minetest.get_meta(pos)
@@ -74,8 +72,96 @@ local function push_text(lines, text, maxlines, maxcolumns)
 end
 
 function digiterms.push_text_on_screen(pos, text)
-  local lines = get_lines(pos)
-  push_text(lines, text, 6, digiterms.columns)
-  set_lines(pos, lines)
-  display_api.update_entities(pos)
+  local def = minetest.registered_nodes[minetest.get_node(pos).name]
+  if def.display_entities and def.display_entities["digiterms:screen"] then
+    def = def.display_entities["digiterms:screen"]
+    if def.lines and def.columns then
+      local lines = get_lines(pos)
+      push_text(lines, text, def.lines, def.columns)
+      set_lines(pos, lines)
+      display_api.update_entities(pos)
+    else
+      minetest.log("warning", "[digiterms] At "..minetest.pos_to_string(pos)
+        ..", digiterms:screen entity should have 'lines' and 'columns' attribures.")
+    end
+  else
+    minetest.log("warning", "[digiterms] Node at "..minetest.pos_to_string(pos)
+      .." does not have digiterms:screen entity.")
+  end
+end
+
+local node_def_defaults = {
+	groups = { display_api = 1},
+	on_place = display_api.on_place,
+	on_destruct = display_api.on_destruct,
+	on_rotate = display_api.on_rotate,
+	on_punch = display_api.update_entities,
+	on_construct = function(pos)
+			minetest.get_meta(pos):set_string("formspec",
+				"field[channel;Channel;${channel}]")
+			display_api.on_construct(pos)
+		end,
+	on_receive_fields = function(pos, formname, fields, player)
+			local name = player:get_player_name()
+			if minetest.is_protected(pos, name) then
+				minetest.record_protection_violation(pos, name)
+				return
+			end
+
+			if (fields.channel) then
+				minetest.get_meta(pos):set_string("channel", fields.channel)
+			end
+		end,
+	digiline = {
+		wire = { use_autoconnect = false },
+		receptor = {},
+		effector = {
+			action = function(pos, _, channel, msg)
+					if channel ~= minetest.get_meta(pos):get_string("channel") then
+						return
+					end
+					digiterms.push_text_on_screen(pos, msg)
+				end,
+		},
+	},
+}
+
+function superpose_table(base, exceptions)
+  local result = table.copy(base)
+  for key, value in pairs(exceptions) do
+		if type(value) == 'table' then
+      result[key] = superpose_table(result[key] or {}, value)
+    else
+      result[key] = value
+    end
+  end
+  return result
+end
+
+function digiterms.register_monitor(
+  nodename, nodedef, nodedefon, nodedefoff)
+  local ndef = superpose_table(node_def_defaults, nodedef)
+  if nodedefon and nodedefoff then
+    ndef.on_punch = function(pos, node)
+      	display_api.on_destruct(pos)
+      	local meta = minetest.get_meta(pos)
+      	meta:set_string("display_text", nil)
+      	minetest.swap_node(pos, {name = nodename..'_off',
+      		param = node.param, param2 = node.param2 })
+    	end
+    minetest.register_node(nodename, superpose_table(ndef, nodedefon))
+
+    -- Register the corresponding Off node
+    ndef.drops = nodename
+  	ndef.groups.not_in_creative_inventory = 1
+    ndef.on_destruct = nil
+  	ndef.on_punch = function(pos, node)
+    		minetest.swap_node(pos, {name = nodename,  -- Stange but it works
+    			param = node.param, param2 = node.param2 })
+    		display_api.update_entities(pos)
+      end
+    minetest.register_node(nodename..'_off', superpose_table(ndef, nodedefoff))
+  else
+    minetest.register_node(nodename, ndef)
+  end
 end
--- nodes.lua
+++ nodes.lua
@@ -18,57 +18,6 @@
 	along with signs.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-local common_node_def = {
-	groups = { display_api = 1},
-	on_place = display_api.on_place,
-	on_destruct = display_api.on_destruct,
-	on_rotate = display_api.on_rotate,
-	on_punch = display_api.update_entities,
-	on_construct = function(pos)
-			minetest.get_meta(pos):set_string("formspec",
-				"field[channel;Channel;${channel}]")
-			display_api.on_construct(pos)
-		end,
-	on_receive_fields = function(pos, formname, fields, player)
-			local name = player:get_player_name()
-			if minetest.is_protected(pos, name) then
-				minetest.record_protection_violation(pos, name)
-				return
-			end
-
-			if (fields.channel) then
-				minetest.get_meta(pos):set_string("channel", fields.channel)
-			end
-		end,
-	digiline = {
-		wire = { use_autoconnect = false },
-		receptor = {},
-		effector = {
-			action = function(pos, _, channel, msg)
-					if channel ~= minetest.get_meta(pos):get_string("channel") then
-						return
-					end
-					digiterms.push_text_on_screen(pos, msg)
-				end,
-		},
-	},
-}
-
-function digiterms.register_monitor(nodename, nodedef)
-	def = table.copy(common_node_def)
-	for key, value in pairs(nodedef) do
-		if key == 'groups' then
-			def[key] = def[key] or {}
-			for key2, value2 in pairs(value) do
-				def[key][key2] = value2
-			end
-		else
-			def[key] = value
-		end
-	end
-	minetest.register_node(nodename, def)
-end
-
 local cathodic_node_box = {
 	type = "fixed",
 	fixed = {
@@ -111,9 +60,6 @@ digiterms.register_monitor('digiterms:lcd_monitor', {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
-	tiles = { "digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
-						"digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
-						"digiterms_lcd_back.png",  "digiterms_lcd_front.png" },
 	drawtype = "nodebox",
 	groups = {choppy = 1, oddly_breakable_by_hand = 1},
 	node_box = lcd_node_box,
@@ -129,36 +75,14 @@ digiterms.register_monitor('digiterms:lcd_monitor', {
 				color = "#203020", font_name = digiterms.font, halign="left", valing="top",
 		},
 	},
-	on_punch = function(pos, node)
-		display_api.on_destruct(pos)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("display_text", nil)
-		minetest.swap_node(pos, {name = 'digiterms:lcd_monitor_off',
-			param = node.param, param2 = node.param2 })
-	end
-})
-
-minetest.register_node('digiterms:lcd_monitor_off', {
-	description = "LCD monitor",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drops = "digiterms:lcd_monitor",
-	sunlight_propagates = false,
+}, {
+	tiles = { "digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
+						"digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
+						"digiterms_lcd_back.png",  "digiterms_lcd_front.png" },
+}, {
 	tiles = { "digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
 						"digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
 						"digiterms_lcd_back.png",  "digiterms_lcd_front_off.png" },
-	drawtype = "nodebox",
-	groups = {choppy = 1, oddly_breakable_by_hand = 1, not_in_creative_inventory = 1},
-	node_box = lcd_node_box,
-	collision_box = lcd_collision_box,
-	selection_box = lcd_collision_box,
-	on_place = display_api.on_place,
-	on_receive_fields = common_node_def.on_receive_fields,
-	on_punch = function(pos, node)
-		minetest.swap_node(pos, {name = 'digiterms:lcd_monitor',
-			param = node.param, param2 = node.param2 })
-		display_api.update_entities(pos)
-  end,
 })
 
 digiterms.register_monitor('digiterms:cathodic_amber_monitor', {
@@ -166,9 +90,6 @@ digiterms.register_monitor('digiterms:cathodic_amber_monitor', {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
-	tiles = { "digiterms_amber_top.png", "digiterms_amber_bottom.png",
-						"digiterms_amber_sides.png", "digiterms_amber_sides.png^[transformFX]",
-						"digiterms_amber_back.png", "digiterms_amber_front.png",},
 	drawtype = "nodebox",
 	groups = {choppy = 1, oddly_breakable_by_hand = 1},
 	node_box = cathodic_node_box,
@@ -184,36 +105,14 @@ digiterms.register_monitor('digiterms:cathodic_amber_monitor', {
 				color = "#FFA000", font_name = digiterms.font, halign="left", valing="top",
 		},
 	},
-	on_punch = function(pos, node)
-		display_api.on_destruct(pos)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("display_text", nil)
-		minetest.swap_node(pos, {name = 'digiterms:cathodic_amber_monitor_off',
-			param = node.param, param2 = node.param2 })
-	end
-})
-
-minetest.register_node('digiterms:cathodic_amber_monitor_off', {
-	description = "Cathodic amber monitor",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drops = "digiterms:cathodic_amber_monitor",
-	sunlight_propagates = false,
-	tiles = { "digiterms_amber_top.png", "digiterms_amber_bottom.png",
+}, {
+	tiles = { "digiterms_amber_top.png",   "digiterms_amber_bottom.png",
 						"digiterms_amber_sides.png", "digiterms_amber_sides.png^[transformFX]",
-						"digiterms_amber_back.png", "digiterms_amber_front_off.png",},
-	drawtype = "nodebox",
-	groups = {choppy = 1, oddly_breakable_by_hand = 1, not_in_creative_inventory = 1},
-	node_box = cathodic_node_box,
-	collision_box = cathodic_collision_box,
-	selection_box = cathodic_collision_box,
-	on_place = display_api.on_place,
-	on_receive_fields = common_node_def.on_receive_fields,
-	on_punch = function(pos, node)
-		minetest.swap_node(pos, {name = 'digiterms:cathodic_amber_monitor',
-			param = node.param, param2 = node.param2 })
-		display_api.update_entities(pos)
-  end,
+						"digiterms_amber_back.png",  "digiterms_amber_front.png",},
+}, {
+	tiles = { "digiterms_amber_top.png",   "digiterms_amber_bottom.png",
+						"digiterms_amber_sides.png", "digiterms_amber_sides.png^[transformFX]",
+						"digiterms_amber_back.png",  "digiterms_amber_front_off.png",},
 })
 
 digiterms.register_monitor('digiterms:cathodic_green_monitor', {
@@ -221,9 +120,6 @@ digiterms.register_monitor('digiterms:cathodic_green_monitor', {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
-	tiles = { "digiterms_green_top.png", "digiterms_green_bottom.png",
-						"digiterms_green_sides.png", "digiterms_green_sides.png^[transformFX]",
-						"digiterms_green_back.png", "digiterms_green_front.png",},
 	drawtype = "nodebox",
 	groups = {choppy = 1, oddly_breakable_by_hand = 1},
 	node_box = cathodic_node_box,
@@ -239,34 +135,12 @@ digiterms.register_monitor('digiterms:cathodic_green_monitor', {
 				color = "#00FF00", font_name = digiterms.font, halign="left", valing="top",
 		},
 	},
-	on_punch = function(pos, node)
-		display_api.on_destruct(pos)
-    local meta = minetest.get_meta(pos)
-		meta:set_string("display_text", nil)
-		minetest.swap_node(pos, {name = 'digiterms:cathodic_green_monitor_off',
-			param = node.param, param2 = node.param2 })
-	end
-})
-
-minetest.register_node('digiterms:cathodic_green_monitor_off', {
-	description = "Cathodic green monitor",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drops = "digiterms:cathodic_green_monitor",
-	sunlight_propagates = false,
+}, {
+	tiles = { "digiterms_green_top.png",   "digiterms_green_bottom.png",
+						"digiterms_green_sides.png", "digiterms_green_sides.png^[transformFX]",
+						"digiterms_green_back.png",  "digiterms_green_front.png",},
+}, {
 	tiles = { "digiterms_green_top.png", "digiterms_green_bottom.png",
 						"digiterms_green_sides.png", "digiterms_green_sides.png^[transformFX]",
 						"digiterms_green_back.png", "digiterms_green_front_off.png",},
-	drawtype = "nodebox",
-	groups = {choppy = 1, oddly_breakable_by_hand = 1, not_in_creative_inventory = 1},
-	node_box = cathodic_node_box,
-	collision_box = cathodic_collision_box,
-	selection_box = cathodic_collision_box,
-	on_place = display_api.on_place,
-	on_receive_fields = common_node_def.on_receive_fields,
-	on_punch = function(pos, node)
-		minetest.swap_node(pos, {name = 'digiterms:cathodic_green_monitor',
-			param = node.param, param2 = node.param2 })
-		display_api.update_entities(pos)
-  end,
 })
