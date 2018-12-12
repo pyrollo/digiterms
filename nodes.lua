1f3a5371cd3fc6f23c8b06251378cac43bf7e56e
Pierre-Yves Rollo <dev@pyrollo.com>
2018-12-12 21:37:43 +0100

    Added some more keyboards and monitors

--- nodes.lua
+++ nodes.lua
@@ -61,7 +61,7 @@ digiterms.register_monitor('digiterms:lcd_monitor', {
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
 	drawtype = "nodebox",
-	groups = {choppy = 1, oddly_breakable_by_hand = 3},
+	groups = {oddly_breakable_by_hand = 3},
 	node_box = lcd_node_box,
 	collision_box = lcd_collision_box,
 	selection_box = lcd_collision_box,
@@ -84,9 +84,8 @@ digiterms.register_monitor('digiterms:lcd_monitor', {
 						"digiterms_lcd_sides.png", "digiterms_lcd_sides.png",
 						"digiterms_lcd_back.png",  "digiterms_lcd_front_off.png" },
 })
-
-digiterms.register_monitor('digiterms:cathodic_amber_monitor', {
-	description = "Cathodic amber monitor",
+digiterms.register_monitor('digiterms:cathodic_beige_monitor', {
+	description = "Beige cathodic monitor with amber screen",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
@@ -106,22 +105,24 @@ digiterms.register_monitor('digiterms:cathodic_amber_monitor', {
 		},
 	},
 }, {
-	tiles = { "digiterms_amber_top.png",   "digiterms_amber_bottom.png",
-						"digiterms_amber_sides.png", "digiterms_amber_sides.png^[transformFX]",
-						"digiterms_amber_back.png",  "digiterms_amber_front.png",},
+	tiles = { "digiterms_beige_top.png",   "digiterms_beige_bottom.png",
+						"digiterms_beige_sides.png", "digiterms_beige_sides.png^[transformFX]",
+						"digiterms_beige_back.png",  "digiterms_beige_front.png",},
 }, {
-	tiles = { "digiterms_amber_top.png",   "digiterms_amber_bottom.png",
-						"digiterms_amber_sides.png", "digiterms_amber_sides.png^[transformFX]",
-						"digiterms_amber_back.png",  "digiterms_amber_front_off.png",},
+	tiles = { "digiterms_beige_top.png",   "digiterms_beige_bottom.png",
+						"digiterms_beige_sides.png", "digiterms_beige_sides.png^[transformFX]",
+						"digiterms_beige_back.png",  "digiterms_beige_front_off.png",},
 })
+minetest.register_alias('digiterms:cathodic_amber_monitor', 'digiterms:cathodic_beige_monitor')
+minetest.register_alias('digiterms:cathodic_amber_monitor_off', 'digiterms:cathodic_beige_monitor_off')
 
-digiterms.register_monitor('digiterms:cathodic_green_monitor', {
-	description = "Cathodic green monitor",
+digiterms.register_monitor('digiterms:cathodic_white_monitor', {
+	description = "White cathodic monitor with green screen",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
 	drawtype = "nodebox",
-	groups = {oddly_breakable_by_hand = 1, dig_immediate = 1},
+	groups = {oddly_breakable_by_hand = 3},
 	node_box = cathodic_node_box,
 	collision_box = cathodic_collision_box,
 	selection_box = cathodic_collision_box,
@@ -136,71 +137,83 @@ digiterms.register_monitor('digiterms:cathodic_green_monitor', {
 		},
 	},
 }, {
-	tiles = { "digiterms_green_top.png",   "digiterms_green_bottom.png",
-						"digiterms_green_sides.png", "digiterms_green_sides.png^[transformFX]",
-						"digiterms_green_back.png",  "digiterms_green_front.png",},
+	tiles = { "digiterms_white_top.png",   "digiterms_white_bottom.png",
+						"digiterms_white_sides.png", "digiterms_white_sides.png^[transformFX]",
+						"digiterms_white_back.png",  "digiterms_white_front.png",},
 }, {
-	tiles = { "digiterms_green_top.png", "digiterms_green_bottom.png",
-						"digiterms_green_sides.png", "digiterms_green_sides.png^[transformFX]",
-						"digiterms_green_back.png", "digiterms_green_front_off.png",},
+	tiles = { "digiterms_white_top.png", "digiterms_white_bottom.png",
+						"digiterms_white_sides.png", "digiterms_white_sides.png^[transformFX]",
+						"digiterms_white_back.png", "digiterms_white_front_off.png",},
 })
+minetest.register_alias('digiterms:cathodic_green_monitor', 'digiterms:cathodic_white_monitor')
+minetest.register_alias('digiterms:cathodic_green_monitor_off', 'digiterms:cathodic_white_monitor_off')
 
-minetest.register_node('digiterms:beige_keyboard', {
-	description = "Beige keyboard",
+digiterms.register_monitor('digiterms:cathodic_black_monitor', {
+	description = "Black cathodic monitor with white screen",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = false,
-	tiles = { "digiterms_beige_keyboard_top.png",   "digiterms_beige_keyboard_bottom.png",
-						"digiterms_beige_keyboard_sides.png", "digiterms_beige_keyboard_sides.png",
-						"digiterms_beige_keyboard_sides.png", "digiterms_beige_keyboard_sides.png",},
 	drawtype = "nodebox",
-	groups = {choppy = 1, oddly_breakable_by_hand = 1},
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-8/16,  -8/16, -1/16, 8/16, -6/16, 7/16},
-			{-7/16,  -12/32, 0, 7/16, -11/32, 6/16},
-		}
+	groups = {oddly_breakable_by_hand = 3},
+	node_box = cathodic_node_box,
+	collision_box = cathodic_collision_box,
+	selection_box = cathodic_collision_box,
+	display_entities = {
+		["digiterms:screen"] = {
+				on_display_update = font_api.on_display_update,
+				depth = -7/16 - display_api.entity_spacing,
+				top = -1/16,
+				size = { x = 23/32, y = 10/16 },
+				columns = 20, lines = 6,
+				color = "#D0D0D0", font_name = digiterms.font, halign="left", valing="top",
+		},
 	},
-	digiline =
-	{
-		receptor = {}
-	},
-	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-			if not minetest.is_player(player) then
-				return
-			end
-			local name = player:get_player_name()
-			local context = digiterms.get_player_context(name)
-			context.formname = 'digiterms:keyboard'
-			context.pos = pos
-
-			local owned = not minetest.is_protected(context.pos, name)
-			local protected = minetest.is_protected(context.pos, '')
-			local meta = minetest.get_meta(pos)
-			local channel = meta:get_string('channel')
-			local public = meta:get_string('public')
-
-			if owned then
-				local fs = "size[8,5]"..
-					default.gui_bg..default.gui_bg_img..default.gui_slots..
-					"field[1,1;3,1;channel;Chanel;"..channel.."]"..
-					"field[1,3;6.5,1;text;Type text:;]"..
-					"field_close_on_enter[text;true]button_exit[2.5,4;3,1;send;Send]"
-				if protected then
-					fs = fs.."checkbox[4,0.6;public;Public keyboard;"..public.."]"
-				end
-				minetest.show_formspec(name, context.formname, fs)
-			else
-				if public == 'true' and channel ~= '' then
-					minetest.show_formspec(name, context.formname, "size[8,3]"..
-						default.gui_bg..default.gui_bg_img..default.gui_slots..
-						"field[1,1;6.5,1;text;Type text:;]"..
-						"field_close_on_enter[text;true]button_exit[2.5,2;3,1;send;Send]")
-				end
-			end
-		end,
+}, {
+	tiles = { "digiterms_black_top.png",   "digiterms_black_bottom.png",
+						"digiterms_black_sides.png", "digiterms_black_sides.png^[transformFX]",
+						"digiterms_black_back.png",  "digiterms_black_front.png",},
+}, {
+	tiles = { "digiterms_black_top.png", "digiterms_black_bottom.png",
+						"digiterms_black_sides.png", "digiterms_black_sides.png^[transformFX]",
+						"digiterms_black_back.png", "digiterms_black_front_off.png",},
 })
+
+-- KEYBOARDS
+
+local keyboard_on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+	if not minetest.is_player(player) then
+		return
+	end
+	local name = player:get_player_name()
+	local context = digiterms.get_player_context(name)
+	context.formname = 'digiterms:keyboard'
+	context.pos = pos
+
+	local owned = not minetest.is_protected(context.pos, name)
+	local protected = minetest.is_protected(context.pos, '')
+	local meta = minetest.get_meta(pos)
+	local channel = meta:get_string('channel')
+	local public = meta:get_string('public')
+
+	if owned then
+		local fs = "size[8,5]"..
+			default.gui_bg..default.gui_bg_img..default.gui_slots..
+			"field[1,1;3,1;channel;Chanel;"..channel.."]"..
+			"field[1,3;6.5,1;text;Type text:;]"..
+			"field_close_on_enter[text;true]button_exit[2.5,4;3,1;send;Send]"
+		if protected then
+			fs = fs.."checkbox[4,0.6;public;Public keyboard;"..public.."]"
+		end
+		minetest.show_formspec(name, context.formname, fs)
+	else
+		if public == 'true' and channel ~= '' then
+			minetest.show_formspec(name, context.formname, "size[8,3]"..
+				default.gui_bg..default.gui_bg_img..default.gui_slots..
+				"field[1,1;6.5,1;text;Type text:;]"..
+				"field_close_on_enter[text;true]button_exit[2.5,2;3,1;send;Send]")
+		end
+	end
+end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 		if formname ~= 'digiterms:keyboard' or not minetest.is_player(player) then
@@ -232,3 +245,76 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		end
 		return true
 	end)
+
+
+minetest.register_node('digiterms:beige_keyboard', {
+	description = "Beige keyboard",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = false,
+	tiles = { "digiterms_beige_keyboard_top.png",   "digiterms_beige_keyboard_bottom.png",
+						"digiterms_beige_keyboard_sides.png", "digiterms_beige_keyboard_sides.png",
+						"digiterms_beige_keyboard_sides.png", "digiterms_beige_keyboard_sides.png",},
+	drawtype = "nodebox",
+	groups = {oddly_breakable_by_hand = 3},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-8/16,  -8/16, -1/16, 8/16, -6/16, 7/16},
+			{-7/16,  -12/32, 0, 7/16, -11/32, 6/16},
+		}
+	},
+	on_rightclick = keyboard_on_rightclick,
+	digiline =
+	{
+		receptor = {}
+	},
+})
+
+minetest.register_node('digiterms:white_keyboard', {
+	description = "White keyboard",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = false,
+	tiles = { "digiterms_white_keyboard_top.png",   "digiterms_white_keyboard_bottom.png",
+						"digiterms_white_keyboard_sides.png", "digiterms_white_keyboard_sides.png",
+						"digiterms_white_keyboard_sides.png", "digiterms_white_keyboard_sides.png",},
+	drawtype = "nodebox",
+	groups = {oddly_breakable_by_hand = 3},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-8/16,  -8/16, -1/16, 8/16, -6/16, 7/16},
+			{-7/16,  -12/32, 0, 7/16, -11/32, 6/16},
+		}
+	},
+	on_rightclick = keyboard_on_rightclick,
+	digiline =
+	{
+		receptor = {}
+	},
+})
+
+minetest.register_node('digiterms:black_keyboard', {
+	description = "Black keyboard",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = false,
+	tiles = { "digiterms_black_keyboard_top.png",   "digiterms_black_keyboard_bottom.png",
+						"digiterms_black_keyboard_sides.png", "digiterms_black_keyboard_sides.png",
+						"digiterms_black_keyboard_sides.png", "digiterms_black_keyboard_sides.png",},
+	drawtype = "nodebox",
+	groups = {oddly_breakable_by_hand = 3},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-8/16,  -8/16, -1/16, 8/16, -6/16, 7/16},
+			{-7/16,  -12/32, 0, 7/16, -11/32, 6/16},
+		}
+	},
+	on_rightclick = keyboard_on_rightclick,
+	digiline =
+	{
+		receptor = {}
+	},
+})
